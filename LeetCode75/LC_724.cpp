7669b172ea52602f32dc7fe5de49c2d4e9c6cb2f
Yeahn Kim <yeahnkim@gmail.com>
2025-04-28 22:02:55 -0700

    724

---
+++ LeetCode75/LC_724.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int left=0, right=accumulate(nums.begin(), nums.end(), 0);
+        for(int i=0; i<nums.size(); i++) {
+            if(i>0) {
+                left+=nums[i-1];
+            }
+            if(i<nums.size()) {
+                right-=nums[i];
+            }
+            if(left==right) return i;
+        }
+        return -1;
+    }
+};
